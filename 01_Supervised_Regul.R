e5e89538355c5d2ca61bf6490c470416e16a8639
glezma <gonzalolezma@gmail.com>
2020-10-10 00:58:31 -0500

    application1

--- 01_Supervised_Regul.R
+++ 01_Supervised_Regul.R
@@ -1,3 +1,4 @@
+
 library(tidyverse)
 library(caret)
 library(glmnet)
@@ -21,18 +22,40 @@ test.data = predict(pre_proc_val, test.data.raw)
 x <- model.matrix(medv~., train.data)[,-1]
 # Outcome variable
 y <- train.data$medv
-# Ridge regression
+
+
+# 1 Ridge Regression
 
 # Find the best lambda using cross-validation
 set.seed(123) 
 cv <- cv.glmnet(x, y, alpha = 0)
 # Display the best lambda value
 cv$lambda.min
-
 # Fit the final model on the training data
 model <- glmnet(x, y, alpha = 0, lambda = cv$lambda.min)
 # Display regression coefficients
-coef(model)
+# coef(model)
+
+# Make predictions on the test data
+x.test <- model.matrix(medv ~., test.data)[,-1]
+predictions <- model %>% predict(x.test) %>% as.vector()
+# Model performance metrics on the test data
+data.frame(
+  RMSE = RMSE(predictions, test.data$medv),
+  Rsquare = R2(predictions, test.data$medv)
+)
+
+# 2 Lasso
+
+# Find the best lambda using cross-validation
+set.seed(123) 
+cv <- cv.glmnet(x, y, alpha = 1)
+# Display the best lambda value
+cv$lambda.min
+# Fit the final model on the training data
+model <- glmnet(x, y, alpha = 1, lambda = cv$lambda.min)
+# Dsiplay regression coefficients
+# coef(model)
 
 # Make predictions on the test data
 x.test <- model.matrix(medv ~., test.data)[,-1]
@@ -42,3 +65,65 @@ data.frame(
   RMSE = RMSE(predictions, test.data$medv),
   Rsquare = R2(predictions, test.data$medv)
 )
+
+
+# 3 Elastic NEt
+# Build the model using the training set
+set.seed(123)
+model <- train(
+  medv ~., data = train.data, method = "glmnet",
+  trControl = trainControl("cv", number = 10),
+  tuneLength = 10
+)
+# Best tuning parameter
+model$bestTune
+
+# Coefficient of the final model. You need
+# to specify the best lambda
+# coef(model$finalModel, model$bestTune$lambda)
+
+#Make predictions on the test data
+x.test <- model.matrix(medv ~., test.data)[,-1]
+predictions <- model %>% predict(x.test)
+# Model performance metrics
+data.frame(
+  RMSE = RMSE(predictions, test.data$medv),
+  Rsquare = R2(predictions, test.data$medv)
+)
+
+# Comparison
+
+# Comparing all model.response(
+lambda <- 10^seq(2, 3, length = 10)
+
+
+# Build the model
+set.seed(123)
+ridge <- train(
+  medv ~., data = train.data, method = "glmnet",
+  trControl = trainControl("cv", number = 10),
+  tuneGrid = expand.grid(alpha = 0, lambda = lambda)
+)
+
+# LASSO
+
+# Build the model
+set.seed(123)
+lasso <- train(
+  medv ~., data = train.data, method = "glmnet",
+  trControl = trainControl("cv", number = 10),
+  tuneGrid = expand.grid(alpha = 1, lambda = lambda)
+)
+
+# Build the model
+set.seed(123)
+elastic <- train(
+  medv ~., data = train.data, method = "glmnet",
+  trControl = trainControl("cv", number = 10),
+  tuneLength = 10
+)
+
+models <- list(ridge = ridge, lasso = lasso, elastic = elastic)
+resamples(models) %>% summary( metric = "RMSE")
+
+
